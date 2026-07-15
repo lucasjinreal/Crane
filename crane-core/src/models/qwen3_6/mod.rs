ee454f161d1fadefe7ba186f7404b015c9fca643
lumos1 <jfg@qq.com>
2026-07-15 13:44:17 +0800

    add some details

--- crane-core/src/models/qwen3_6/mod.rs
+++ crane-core/src/models/qwen3_6/mod.rs
@@ -2,3 +2,5 @@
 // Qwen3.6 27B with Bonsai Ternary could be runing on Desktop withonly 5GB mem
 // while kept about 95% original ability
 // https://huggingface.co/prism-ml/Ternary-Bonsai-27B-gguf/tree/main
+// Qwen3.6 holds almost identical arch as Qwen3.5
+// We need consider support MTP & DSpark in this version.
